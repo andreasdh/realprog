0175462ce4c9c2104636e3ecf5f167083223c007
Odd Petter Sand <o.p.sand@astro.uio.no>
2024-03-22 15:26:00 +0100

    Oppgaver om varians

---
+++ testkode/varians.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+soldata = pd.read_csv("../docs/datafiler/solflekker.txt", delimiter="\t")
+
+flekkdata = soldata["Solflekker"]
+n = len(flekkdata) # antall målinger
+
+sum = 0
+for flekker in flekkdata:
+    sum += flekker
+snitt = sum/n
+print("Gjennomsnitt:", round(snitt, 1))
+
+sum_avvik = 0
+for flekker in flekkdata:
+    sum_avvik += (flekker - snitt) # positivt over snitt, negativt under snitt
+snitt_avvik = sum_avvik/n
+print("Gjennomsnittlig avvik:", round(snitt_avvik, 1))
+
+sum_avvik_2 = 0
+for flekker in flekkdata:
+    sum_avvik_2 += (flekker - snitt)**2 # alltid positivt
+snitt_avvik_2 = sum_avvik_2/n # gjennomsnittlig kvadratavvik i DATASETTET vårt
+
+varians = sum_avvik_2/(n-1) # (estimert) gjennomsnittlig kvadratavvik i HELE POPULASJONEN
+# hvorfor (n-1)?
+# https://en.wikipedia.org/wiki/Bessel%27s_correction#Proof_of_correctness_%E2%80%93_Alternative_1
+print("Varians:", round(varians, 1))
+print("Standardavvik:", round(varians**0.5, 1))
